3ddfd745106002032536418ef52c619ff5bfa451
ASMO360 <uafredrickson@gmail.com>
2017-11-13 15:13:28 -0800

    added objects

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 'use strict';
+
+
+//1st and Pike Store
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3
+  custPerHr: function() {
+    return Math.random() * (this.minCust, this.maxCust) + this.maxCust;
+  };
+};
+
+// SeaTac Airport
+var seaTac = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2
+};
+
+//Seattle Center
+var seaCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7
+};
+
+//Capitol Hill
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3
+};
+
+//Alki
+var Alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6
+};
